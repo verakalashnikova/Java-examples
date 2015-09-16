7d6184a3be4437ce84cb7986fd351df2c246bc56
vera <vera.kalashnikova@gmail.com>
2015-09-16 13:08:16 -0400

    Dependency Injection example

---
+++ src/com/journaldev/java/dependencyinjection/consumer/MyDIApplication.java
@@ -0,0 +1,27 @@
+package com.journaldev.java.dependencyinjection.consumer;
+
+import com.journaldev.java.dependencyinjection.service.MessageService;
+
+public class MyDIApplication implements Consumer{
+
+	private MessageService service;
+	
+	public MyDIApplication(){}
+
+	
+	public MyDIApplication (MessageService svc){
+		this.service=svc;
+	}
+	
+	
+	public void setService(MessageService service) {
+		this.service = service;
+	}
+
+	@Override
+	public void processMessages(String msg, String rec){
+		//do some msg validation, manipulation logic etc
+		this.service.sendMessage(msg, rec);
+	}
+
+}
